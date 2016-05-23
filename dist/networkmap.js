9e68ce91c8874aba5a0100f8267983a32ec4a437
Emil Palm <emil@x86.nu>
2016-05-23 15:24:46 +0200

    Removing the dependency of twitter-boostrap

--- dist/networkmap.js
+++ dist/networkmap.js
@@ -11762,7 +11762,7 @@ networkMap.extend(networkMap.Link.Module.Edge, {
 ;networkMap.drawingCallbacks = networkMap.drawingCallbacks || {};
 
 /**
- * Register a drawing callbakc
+ * Register a drawing callback
  *
  * @param {string} name The name of the drawing callback used in node options.
  * @param {function} f The callback to do svg drawing.
